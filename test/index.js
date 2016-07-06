50641ed5bab5660a77a19646d7b3acc083396f96
Houfeng <admin@xhou.net>
2016-07-06 18:49:23 +0800

    0.0.8

--- test/index.js
+++ test/index.js
@@ -2,26 +2,24 @@ var shify = require('../');
 
 var io = shify(function () {
   /*
-  echo 'my name is ${name}'
-  echo $PWD
-  ls
+  echo ${name} 
+  nokit ls
   */
 }, { params: { name: 'houfeng' } });
 
-io.on('error', function (err) {
-  //console.error('error', err);
-});
-io.on('success', function () {
-  //console.log('success');
-});
-io.on('close', function (code) {
+// io.on('error', function (err) {
+//   //console.error('error', err);
+// });
+// io.on('success', function () {
+//   //console.log('success');
+// });
+
+io.on('exit', function (code) {
   console.info('exit', code);
 });
 
-io.stdout.on('data', function (data) {
-  console.log(data.toString());
-});
+io.stdout.pipe(process.stdout);
 
-io.stderr.on('data', function (data) {
-  console.error(data.toString());
-});
+// io.stderr.on('data', function (data) {
+//   console.error(data.toString());
+// });
