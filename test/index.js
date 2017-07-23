258b726660a3fdd70f9fb975a672561f1a6bcd8c
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-07-23 22:46:17 +0800

    2.0.0beta1

--- test/index.js
+++ test/index.js
@@ -1,26 +1,29 @@
 var shify = require('../');
 
-var io = shify(function () {
-  /*
-  echoa
-  echo houfeng
-  */
-}, { params: { name: 'houfeng' }, temp: false });
+var io = shify(`
+set -e
+echo houfeng1 
+echo houfeng2
+`, { flags2: ['/d', '/s', '/c', 'bash'] });
 
 // io.on('error', function (err) {
-//   //console.error('error', err);
+//   console.error('error', err);
 // });
 // io.on('success', function () {
-//   //console.log('success');
+//   console.log('success');
+// });
+
+//io.stdout.pipe(process.stdout);
+//io.stderr.pipe(process.stderr);
+
+io.stdout.on('data', function (data) {
+  console.log(data.toString());
+});
+
+// io.stderr.on('data', function (data) {
+//   console.error('error:', data.toString());
 // });
 
 io.on('exit', function (code) {
-  console.info('exit', code);
+  console.log('exit:', code);
 });
-
-io.stdout.pipe(process.stdout);
-//io.stderr.pipe(process.stderr);
-
-// io.stderr.on('data', function (data) {
-//   console.error(data.toString());
-// });
